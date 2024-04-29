638c5216fb2f8303d306e09efc3c9060448ab899
abdelrahman-sameeh <abdelrahman.sameeh507@gmail.com>
2024-04-29 03:09:26 +0300

    add cors origin in backend

--- backend/src/settings.py
+++ backend/src/settings.py
@@ -28,9 +28,10 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "rest_framework",
     "rest_framework_simplejwt",
+    "corsheaders",
     "users",
     "mailer",
-    'todos',
+    "todos",
 ]
 
 MIDDLEWARE = [
@@ -41,6 +42,11 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "corsheaders.middleware.CorsMiddleware",
+]
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:8080",
 ]
 
 
